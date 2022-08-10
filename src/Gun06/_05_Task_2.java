52acb3618c8171ace93785809cdb3fa24f5f1c60
Cloud474 <huseyin.blt47@hotmail.com>
2022-08-10 09:36:49 +0300

    new1

--- src/Gun06/_05_Task_2.java
+++ src/Gun06/_05_Task_2.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.List;
